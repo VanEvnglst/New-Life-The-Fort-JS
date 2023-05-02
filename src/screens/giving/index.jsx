aaeb81fbfc6bb77a577cd6578b1edb196b162f86
Ivan <engr.ivan.evangelista@gmail.com>
2023-05-02 20:10:21 +0800

    create placeholder screen

---
+++ src/screens/giving/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const GivingScreen = () => {
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{ fontSize: 24, fontWeight: '600'}}>Giving gateway</Text>
+    </View>
+  )
+}
+
+export default GivingScreen;
